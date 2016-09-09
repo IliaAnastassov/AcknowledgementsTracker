175c46b74ad3d2d728563650b33afaf65fa808fa
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-09 14:50:52 +0300

    Add Tag class and ITag interface

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Model/Contracts/ITag.cs
@@ -0,0 +1,9 @@
+﻿namespace AcknowledgementsTracker.Model.Contracts
+{
+    public interface ITag
+    {
+        int Id { get; set; }
+
+        string Title { get; set; }
+    }
+}
---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Model/Models/Tag.cs
@@ -0,0 +1,23 @@
+﻿namespace AcknowledgementsTracker.Model.Models
+{
+    using System;
+    using Contracts;
+
+    public class Tag : ITag
+    {
+        private int id;
+        private string title;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+    }
+}
