c60d4772aadf865c599babbd747d061943f920b2
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-10 17:26:13 +0300

    Update ConsoleApplication

--- AcknowledgementsTracker/ConsoleApplication/Startup.cs
+++ AcknowledgementsTracker/ConsoleApplication/Startup.cs
@@ -1,13 +1,20 @@
 ﻿namespace ConsoleApplication
 {
     using System;
+    using System.Data.Entity;
     using AcknowledgementsTracker.Buisiness.Models;
+    using AcknowledgementsTracker.DataAccess;
 
     class Startup
     {
         static void Main(string[] args)
         {
+            Database.SetInitializer(new NullDatabaseInitializer<AcknowledgementsTrackerContext>());
 
+            var modelManager = new ModelManager();
+            ////modelManager.CreateProxiadEmployee("shmuleyB", "shmuley@proxiad.com");
+            ////modelManager.CreateTag("mvvm");
+            modelManager.CreateAcknowledgement("Thanks bro!", 1);
         }
     }
 }
