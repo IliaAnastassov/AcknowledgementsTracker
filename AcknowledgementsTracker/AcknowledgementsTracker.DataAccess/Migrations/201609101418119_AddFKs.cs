700049798fec77aa50ba0d0561dc3980c4d75724
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-10 17:21:48 +0300

    Add db migration

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.DataAccess/Migrations/201609101418119_AddFKs.cs
@@ -0,0 +1,16 @@
+namespace AcknowledgementsTracker.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFKs : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
