935a5f72e5f78dba7e51a29359046b22ce0fe86a
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-10 17:21:24 +0300

    Update ModelManager class and coresponding interface

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Buisiness/Contracts/IModelManager.cs
@@ -0,0 +1,23 @@
+﻿namespace AcknowledgementsTracker.Buisiness.Contracts
+{
+    public interface IModelManager
+    {
+        void CreateAcknowledgement(string text, int id);
+
+        void CreateProxiadEmployee(string userName, string email);
+
+        void CreateTag(string title);
+
+        void DeleteAcknowledgement();
+
+        void DeleteProxiadEmployee();
+
+        void DeleteTag();
+
+        void EditAcknowledgement();
+
+        void EditProxiadEmployee();
+
+        void EditTag();
+    }
+}
---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Buisiness/Models/ModelManager.cs
@@ -0,0 +1,88 @@
+﻿namespace AcknowledgementsTracker.Buisiness.Models
+{
+    using System;
+    using Contracts;
+    using DataAccess;
+    using Model.Models;
+
+    public class ModelManager : IModelManager
+    {
+        public void CreateAcknowledgement(string text, int employeeId)
+        {
+            var acknowledgement = new Acknowledgement()
+            {
+                Text = text,
+                DateCreated = DateTime.Now,
+                ProxiadEmployeeId = employeeId
+            };
+
+            using (var context= new AcknowledgementsTrackerContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                context.Acknowledgements.Add(acknowledgement);
+                context.SaveChanges();
+            }
+        }
+
+        public void CreateProxiadEmployee(string userName, string email)
+        {
+            var employee = new ProxiadEmployee()
+            {
+                UserName = userName,
+                Email = email
+            };
+
+            using (var context = new AcknowledgementsTrackerContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                context.ProxiadEmployees.Add(employee);
+                context.SaveChanges();
+            }
+        }
+
+        public void CreateTag(string title)
+        {
+            var tag = new Tag()
+            {
+                Title = $"#{title}"
+            };
+
+            using (var context = new AcknowledgementsTrackerContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                context.Tags.Add(tag);
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteAcknowledgement()
+        {
+            // TODO:
+        }
+
+        public void DeleteProxiadEmployee()
+        {
+            // TODO:
+        }
+
+        public void DeleteTag()
+        {
+            // TODO:
+        }
+
+        public void EditAcknowledgement()
+        {
+            // TODO:
+        }
+
+        public void EditProxiadEmployee()
+        {
+            // TODO:
+        }
+
+        public void EditTag()
+        {
+            // TODO:
+        }
+    }
+}
