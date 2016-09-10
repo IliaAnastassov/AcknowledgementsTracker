53620b7bb67dac4babe291286e7ca892856b9708
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-10 17:20:54 +0300

    Add database migrations

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.DataAccess/Migrations/201609101354388_AddTagsFKToAcknowledgement.cs
@@ -0,0 +1,16 @@
+namespace AcknowledgementsTracker.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTagsFKToAcknowledgement : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
