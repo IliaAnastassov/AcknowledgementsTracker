1aa65142400817429afc97b2750c50ba829693d2
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-10 18:19:49 +0300

    Add TODOs

--- AcknowledgementsTracker/ConsoleApplication/Startup.cs
+++ AcknowledgementsTracker/ConsoleApplication/Startup.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.Data.Entity;
+    using System.Collections.Generic;
     using AcknowledgementsTracker.Buisiness.Models;
     using AcknowledgementsTracker.DataAccess;
 
     class Startup
     {
+        // TODO: Make the many to many relationship work (Acknowledgement - Tag)
         static void Main(string[] args)
         {
             Database.SetInitializer(new NullDatabaseInitializer<AcknowledgementsTrackerContext>());
@@ -14,7 +16,7 @@
             var modelManager = new ModelManager();
             ////modelManager.CreateProxiadEmployee("shmuleyB", "shmuley@proxiad.com");
             ////modelManager.CreateTag("mvvm");
-            modelManager.CreateAcknowledgement("Thanks bro!", 1);
+            modelManager.CreateAcknowledgement("Thanks bro!", 2, new List<int> { 1, 3 });
         }
     }
 }
