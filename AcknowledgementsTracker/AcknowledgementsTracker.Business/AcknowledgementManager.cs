da5c13b6ea0d15821ef164138d0f655a248d9da2
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-14 18:18:12 +0300

    Add Acknowledgement Manager Class

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/AcknowledgementManager.cs
@@ -0,0 +1,24 @@
+﻿namespace AcknowledgementsTracker.Business
+{
+    using System.Collections.Generic;
+    using Model.Models;
+    using DataAccess.Interfaces;
+    using DataAccess.Repositories;
+
+    public class AcknowledgementManager
+    {
+        public void CreateAcknowledgement(string text, int authorId, int beneficiaryId, ICollection<Tag> tags)
+        {
+            var acknowledgement = new Acknowledgement
+            {
+                Text = text,
+                AuthorId = authorId,
+                BeneficiaryId = beneficiaryId,
+                Tags = tags
+            };
+
+            IAcknowledgementsRepository acknowledgementsRepo = new AcknowledgementsRepository();
+            acknowledgementsRepo.SaveAcknowledgement(acknowledgement);
+        }
+    }
+}
