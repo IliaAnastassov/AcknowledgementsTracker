091a1900bd4da5b352a7db94c391139dea6ce4d4
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-14 17:00:17 +0300

    Add tests to ConsoleApplication

--- AcknowledgementsTracker/ConsoleApplication/Startup.cs
+++ AcknowledgementsTracker/ConsoleApplication/Startup.cs
@@ -1,16 +1,64 @@
-﻿namespace ConsoleApplication
+﻿// <copyright file="Startup.cs" company="Proxiad Bulgaria">
+//     Copyright (c) Proxiad Bulgaria. All rights reserved.
+// </copyright>
+// <author>Ilia Anastassov</author>
+//-----------------------------------------------------------------------
+namespace ConsoleApplication
 {
     using System;
     using System.Data.Entity;
-    using System.Collections.Generic;
+    using AcknowledgementsTracker.Business;
     using AcknowledgementsTracker.DataAccess;
+    using AcknowledgementsTracker.DataAccess.Repositories;
 
-    class Startup
+    /// <summary>
+    /// This is a testing application
+    /// </summary>
+    public class Startup
     {
-        // TODO: Make the many to many relationship work (Acknowledgement - Tag)
-        static void Main(string[] args)
+        // TODO: Review multiple Include statements in Repositories
+
+        /// <summary>
+        /// Uncomment any bundle of code in order to test a repository method
+        /// </summary>
+        public static void Main()
         {
             Database.SetInitializer(new NullDatabaseInitializer<AcknowledgementsTrackerContext>());
+
+            var acknowledgementsRepo = new AcknowledgementsRepository();
+            var employeesRepo = new ProxiadEmployeesRepository();
+            var tagsRepo = new TagsRepository();
+            var printer = new Printer();
+
+            // Wolverine's acknowledgements
+            ////int employeeId = 5;
+            ////var acknowledgements = acknowledgementsRepo.GetAcknowledgements(employeeId);
+            ////var employee = employeesRepo.GetProxiadEmployee(employeeId);
+            ////Console.WriteLine($"{employee.UserName}'s acknowledgements:\n");
+            ////printer.PrintAcknowledgements(acknowledgements);
+
+            // Today's acknowledgements
+            ////var todaysAcknowledgements = acknowledgementsRepo.GetTodaysAcknowledgements();
+            ////Console.WriteLine("Today's acknowledgements:\n");
+            ////printer.PrintAcknowledgements(todaysAcknowledgements);
+
+            // This week's acknowledgements
+            ////var thisWeekAcknowledgements = acknowledgementsRepo.GetThisWeekAcknowledgements();
+            ////Console.WriteLine("This week's acknowledgements:\n");
+            ////printer.PrintAcknowledgements(thisWeekAcknowledgements);
+
+            // This month's acknowledgements
+            ////var thisMonthAcknowledgements = acknowledgementsRepo.GetThisMonthAcknowledgements();
+            ////Console.WriteLine("This month's acknowledgements:\n");
+            ////printer.PrintAcknowledgements(thisWeekAcknowledgements);
+
+            // Last acknowledgements
+            ////var lastAcknowledgements = acknowledgementsRepo.GetLastAcknowledgements();
+            ////Console.WriteLine("Last acknowledgements:\n");
+            ////printer.PrintAcknowledgements(lastAcknowledgements);
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
