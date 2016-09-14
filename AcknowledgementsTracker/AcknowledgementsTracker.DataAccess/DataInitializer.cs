c078ab58aaa592eda829ab9b2932b43f61f191bc
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-14 14:02:43 +0300

    Add  content for db initialization

--- AcknowledgementsTracker/AcknowledgementsTracker.DataAccess/DataInitializer.cs
+++ AcknowledgementsTracker/AcknowledgementsTracker.DataAccess/DataInitializer.cs
@@ -9,13 +9,12 @@ namespace AcknowledgementsTracker.DataAccess
     using System.Data.Entity;
     using System.Linq;
     using Model.Models;
+    using System.Collections.Generic;
 
     public class DataInitializer
     {
         public void InitializeData()
         {
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<AcknowledgementsTrackerContext>());
-
             using (var context = new AcknowledgementsTrackerContext())
             {
                 if (context.Acknowledgements.Any() || context.Tags.Any() || context.ProxiadEmployees.Any())
@@ -23,48 +22,89 @@ namespace AcknowledgementsTracker.DataAccess
                     return;
                 }
 
-                var csharpTag = new Tag()
+                // TAGS
+                var weatherTag = new Tag()
                 {
-                    Title = "c#",
+                    Title = "weather",
                 };
 
-                var aspnetTag = new Tag()
+                var theftTag = new Tag()
                 {
-                    Title = "asp.net"
+                    Title = "theft"
                 };
 
-                var atlasTag = new Tag()
+                var drinkingTag = new Tag()
                 {
-                    Title = "atlas"
+                    Title = "drink"
                 };
 
-                var nugetTag = new Tag()
+                var fightTag = new Tag()
                 {
-                    Title = "nuget"
+                    Title = "fight"
                 };
 
+                // EMPLOYEES
                 var wolverine = new ProxiadEmployee()
                 {
                     UserName = "wolverine",
-                    Email = "wolverine@proxiad.com"
+                    Email = "wolverine@xmen.com"
                 };
 
                 var magneto = new ProxiadEmployee()
                 {
                     UserName = "Magneto",
-                    Email = "magneto@proxiad.com"
+                    Email = "magneto@xmen.com"
                 };
 
                 var storm = new ProxiadEmployee()
                 {
                     UserName = "storm",
-                    Email = "storm@proxiad.com"
+                    Email = "storm@xmen.com"
                 };
 
+                var mystique = new ProxiadEmployee()
+                {
+                    UserName = "mystique",
+                    Email = "mystique@xmen.com"
+                };
+
+                var profesorX = new ProxiadEmployee()
+                {
+                    UserName = "professorX",
+                    Email = "professorX@xmen.com"
+                };
+
+                // ACKNOWLEDGEMENTS
                 var acknowledgementOne = new Acknowledgement()
                 {
-                    Text = "Thank you for your help",
-                    // TODO:
+                    Text = "Thank you for making it rain all weekend. Biatch.",
+                    Author = magneto,
+                    Beneficiary = storm,
+                    Tags = new List<Tag> { weatherTag }
+                };
+
+                var acknowledgementTwo = new Acknowledgement()
+                {
+                    Text = "Thanks for being such an asshole and stealing my bike. If you don't give it back to me in one peace I'll kick your butt!",
+                    Author = wolverine,
+                    Beneficiary = magneto,
+                    Tags = new List<Tag> { theftTag, fightTag }
+                };
+
+                var acknowledgementThree = new Acknowledgement()
+                {
+                    Text = "When I was depressed you were there for me. We had such a nice time drinking till collapse.",
+                    Author = profesorX,
+                    Beneficiary = wolverine,
+                    Tags = new List<Tag> { drinkingTag }
+                };
+
+                var acknowledgementFour = new Acknowledgement()
+                {
+                    Text = "Thanks for impersonating me and making it rain all weekend. Now everyone HATES ME because of you!",
+                    Author = storm,
+                    Beneficiary = mystique,
+                    Tags = new List<Tag> { weatherTag, theftTag }
                 };
             }
         }
