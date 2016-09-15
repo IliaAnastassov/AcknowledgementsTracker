5789062169de6ba47a27391c22bf6d009325b28d
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-15 18:14:18 +0300

    Create interfaces for Employees and Tags Repositories

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.DataAccess/Interfaces/IProxiadEmployeesRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace AcknowledgementsTracker.DataAccess.Interfaces
+{
+    using Model.Models;
+
+    public interface IProxiadEmployeesRepository
+    {
+        ProxiadEmployee GetProxiadEmployee(int id);
+
+        ProxiadEmployee GetMostAcknowledgedPersonAllTime();
+
+        ProxiadEmployee GetMostAcknowledgedPersonOfMonth();
+
+        void SaveProxiadEmployee(string userName, string email);
+
+        void EditProxiadEmployee(ProxiadEmployee employee);
+
+        void DeleteProxiadEmployee(int id);
+    }
+}
---
+++ AcknowledgementsTracker/AcknowledgementsTracker.DataAccess/Interfaces/ITagsRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace AcknowledgementsTracker.DataAccess.Interfaces
+{
+    using System.Collections.Generic;
+    using Model.Models;
+
+    public interface ITagsRepository
+    {
+        List<Tag> GetTags();
+
+        Tag GetTag(int id);
+
+        void SaveTag(string title);
+
+        void EditTag(Tag tag);
+
+        void DeleteTag(int id);
+    }
+}
