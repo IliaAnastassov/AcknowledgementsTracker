d4cb53462c17b7a57f2c80f8da72cb498ad7fff0
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-19 18:17:37 +0300

    Update TODOs

--- AcknowledgementsTracker/ConsoleApplication/Startup.cs
+++ AcknowledgementsTracker/ConsoleApplication/Startup.cs
@@ -16,8 +16,6 @@ namespace ConsoleApplication
     /// </summary>
     public class Startup
     {
-        // TODO: Move the Assembler in a separate project
-        // TODO: Implement the Assembler in order to transform DTOs to Model objects and vice versa
         // TODO: Review multiple Include statements in Repositories
         // TODO: Test Save(), Edit() and Delete() methods in AcknowledgementsRepository
         // TODO: Test Save(), Edit() and Delete() methods in ProxiadEmployeesRepository
