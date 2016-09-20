b956eb363804ff6b91fd7e60fe5ee28e3048386a
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-20 15:32:58 +0300

    Rename ModelManager to ModelService. Make use of factory pattern

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Logic/ModelService.cs
@@ -0,0 +1,21 @@
+﻿namespace AcknowledgementsTracker.Business.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using DataAccess.Interfaces;
+    using DataAccess.Repositories;
+    using DTO;
+    using DTO.Interfaces;
+    using Workflow;
+
+    public class ModelService
+    {
+        DtoFactory factory = new DtoFactory();
+
+        public void Create(IDto dto)
+        {
+            var repository = factory.GetRepository(dto.GetType());
+            repository.Save(dto);
+        }
+    }
+}
