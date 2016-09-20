38ed3efcccfdf24f387f6f1319dd32226e281fff
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-20 15:33:48 +0300

    Implement Factory pattern by adding DtoFactory class and corresponding interface

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Interfaces/IDtoFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace AcknowledgementsTracker.Business.Interfaces
+{
+    using System;
+    using DataAccess.Interfaces;
+    using DTO.Interfaces;
+
+    public interface IDtoFactory
+    {
+        IRepository<IDto> GetRepository(Type type);
+    }
+}
---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Workflow/DtoFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace AcknowledgementsTracker.Business.Workflow
+{
+    using System;
+    using System.Collections.Generic;
+    using DataAccess.Interfaces;
+    using DataAccess.Repositories;
+    using DTO;
+    using DTO.Interfaces;
+    using Interfaces;
+
+    public class DtoFactory : IDtoFactory
+    {
+        private Dictionary<Type, IRepository<IDto>> repositories = new Dictionary<Type, IRepository<IDto>>
+        {
+            { typeof(AcknowledgementDTO), new AcknowledgementsRepository() },
+            { typeof(ProxiadEmployeeDTO), new ProxiadEmployeesRepository() },
+            { typeof(TagDTO), new TagsRepository() }
+        };
+
+        public IRepository<IDto> GetRepository(Type type)
+        {
+            return repositories[type];
+        }
+    }
+}
