f1b2777110614da4622e9bdfb79843d9ea193baf
IliaAnastassov <ilia.anastassov@gmail.com>
2016-09-20 12:18:29 +0300

    Add IRepository<T> interface

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace AcknowledgementsTracker.DataAccess.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface IRepository<T>
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll();
+
+        void Save(T model);
+
+        void Edit(T model);
+
+        void Delete(T model);
+    }
+}
