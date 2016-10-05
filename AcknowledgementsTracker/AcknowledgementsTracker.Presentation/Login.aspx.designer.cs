23d144649042c4036aa0a8141477369553445fd7
IliaAnastassov <ilia.anastassov@gmail.com>
2016-10-05 19:00:57 +0300

    Update NewAcknowledgement page

--- AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Login.aspx.designer.cs
+++ AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Login.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace AcknowledgementsTracker.Presentation {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlButton LoginBtn;
+        
+        /// <summary>
+        /// ErrorLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl ErrorLabel;
     }
 }
--- AcknowledgementsTracker/AcknowledgementsTracker.Presentation/NewAcknowledgement.aspx
+++ AcknowledgementsTracker/AcknowledgementsTracker.Presentation/NewAcknowledgement.aspx
@@ -10,9 +10,6 @@
             <div class="col-sm-11">
                 <h2>New Acknowledgement</h2>
             </div>
-            <div class="col-sm-1">
-                <a href="Login.aspx" class="btn btn-default btn-lg"><i class="glyphicon glyphicon-off"></i></a>
-            </div>
         </div>
     </div>
 
