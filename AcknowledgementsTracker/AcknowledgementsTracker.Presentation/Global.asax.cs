d826178d1dd0738f4b551717cd2c386b31904e07
IliaAnastassov <ilia.anastassov@gmail.com>
2016-10-06 18:03:10 +0300

    Remove unnecessary code from Global.aspx

--- AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Global.asax.cs
+++ AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Global.asax.cs
@@ -14,42 +14,5 @@ namespace AcknowledgementsTracker.Presentation
         protected void Application_Start(object sender, EventArgs e)
         {
         }
-
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
-            string cookieName = FormsAuthentication.FormsCookieName;
-            var cookie = Context.Request.Cookies[cookieName];
-
-            if (cookie == null)
-            {
-                return;
-            }
-
-            FormsAuthenticationTicket authenticationTicket;
-
-            try
-            {
-                authenticationTicket = FormsAuthentication.Decrypt(cookie.Value);
-            }
-            catch (ArgumentException)
-            {
-                return;
-            }
-
-            if (authenticationTicket == null)
-            {
-                return;
-            }
-
-            var groups = authenticationTicket.UserData.Split(new char[] { '|' });
-            var id = new GenericIdentity(authenticationTicket.Name, "LdapAuthentication");
-            var principal = new GenericPrincipal(id, groups);
-            Context.User = principal;
-        }
-
-        private void RegisterRoutes(RouteCollection routes)
-        {
-            routes.MapPageRoute("DashboardRoute", "AcknowledgementsTracker", @"~/Dashboard.aspx");
-        }
     }
 }
