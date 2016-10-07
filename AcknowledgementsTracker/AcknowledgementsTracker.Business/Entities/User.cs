3a0bc29b73629b7deca0be485a8d13cf3bedc9c0
IliaAnastassov <ilia.anastassov@gmail.com>
2016-10-07 18:28:13 +0300

    Add User entity

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace AcknowledgementsTracker.Business.Entities
+{
+    using System;
+    using Interfaces;
+
+    public class User : IUser
+    {
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Interfaces/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace AcknowledgementsTracker.Business.Interfaces
+{
+    public interface IUser
+    {
+        string Email { get; set; }
+
+        string Name { get; set; }
+    }
+}
