1a517a2c2f8023d7ba5bf84be217c8ef6e007212
IliaAnastassov <ilia.anastassov@gmail.com>
2016-10-10 19:11:39 +0300

    Update *Master designer

--- AcknowledgementsTracker/AcknowledgementsTracker.Presentation/AcknowledgementsTracker.Master.designer.cs
+++ AcknowledgementsTracker/AcknowledgementsTracker.Presentation/AcknowledgementsTracker.Master.designer.cs
@@ -40,13 +40,13 @@ namespace AcknowledgementsTracker.Presentation {
         protected global::System.Web.UI.WebControls.Literal UserNameLabel;
         
         /// <summary>
-        /// LoginView control.
+        /// LogoutBtn control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginView LoginView;
+        protected global::System.Web.UI.WebControls.Button LogoutBtn;
         
         /// <summary>
         /// cphMain control.
