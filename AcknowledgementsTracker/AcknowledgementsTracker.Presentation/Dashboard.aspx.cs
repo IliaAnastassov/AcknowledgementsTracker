f86fbdd4e9298dde8349b224fdcb0b0e370cd634
IliaAnastassov <ilia.anastassov@gmail.com>
2016-10-10 19:11:52 +0300

    Update Dashboard page

--- AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Dashboard.aspx.cs
+++ AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Dashboard.aspx.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace AcknowledgementsTracker.Presentation
+﻿namespace AcknowledgementsTracker.Presentation
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+    using Business.Logic;
+
     public partial class Dashboard : System.Web.UI.Page
     {
+        ////private LdapAccountManager manager = new LdapAccountManager(ConfigurationManager.AppSettings.);
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            var username = HttpContext.Current?.User?.Identity.Name;
         }
     }
 }
