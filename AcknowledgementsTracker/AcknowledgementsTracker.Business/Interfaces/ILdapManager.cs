cda1ff88a08bf8b8c42302000a0a1578b90842cb
IliaAnastassov <ilia.anastassov@gmail.com>
2016-10-12 18:13:30 +0300

    Update ILdapManager

--- AcknowledgementsTracker/AcknowledgementsTracker.Business/Interfaces/ILdapManager.cs
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Interfaces/ILdapManager.cs
@@ -3,5 +3,9 @@
     public interface ILdapManager
     {
         bool ValidateUser(string username, string password);
+
+        string GetUserFullname(string baseDn, string username);
+
+        string GetUserEmail(string baseDn, string username);
     }
 }
