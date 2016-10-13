cc890654f7b80659185e6bd443341f0855c7cc60
IliaAnastassov <ilia.anastassov@gmail.com>
2016-10-13 18:44:09 +0300

    Update Master page

--- AcknowledgementsTracker/AcknowledgementsTracker.Presentation/AcknowledgementsTracker.Master.cs
+++ AcknowledgementsTracker/AcknowledgementsTracker.Presentation/AcknowledgementsTracker.Master.cs
@@ -8,20 +8,33 @@
     using System.Web.UI;
     using System.Web.UI.WebControls;
     using Business.Logic;
+    using System.Web.Security;
 
     public partial class AcknowledgementsTracker1 : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (LdapAccountManager.Instance != null)
+            if (LdapAccountManager.HasInstance())
             {
                 LogoutBtn.Visible = true;
+                ////UserNameLabel.Text = $"{LdapAccountManager.Instance.GetUserName()} {LdapAccountManager.Instance.GetUserEmail()}";
+
+                var user = LdapAccountManager.Instance.GetUserData();
+                UserNameLabel.Text = $"{user.Name} email:{user.Email}";
+            }
+            else
+            {
+                FormsAuthentication.SignOut();
             }
         }
 
         protected void LogoutBtn_Click(object sender, EventArgs e)
         {
+            LogoutBtn.Visible = false;
             LdapAccountManager.Instance.Destroy();
+
+            FormsAuthentication.SignOut();
+            FormsAuthentication.RedirectToLoginPage();
         }
     }
 }
