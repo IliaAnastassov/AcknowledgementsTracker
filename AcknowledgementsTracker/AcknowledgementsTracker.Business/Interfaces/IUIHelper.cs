21bdf198316518b03d6a6ad41986d5c445ab518b
IliaAnastassov <ilia.anastassov@gmail.com>
2016-11-02 18:27:34 +0200

    Update Business layer

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Interfaces/IUIHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace AcknowledgementsTracker.Business.Interfaces
+{
+    using System.Collections.Generic;
+    using DTO;
+
+    public interface IUIHelper
+    {
+        string GetUserFullName(string username);
+
+        IEnumerable<string> GetTags(IEnumerable<TagDTO> tags);
+    }
+}
---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Logic/UIHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace AcknowledgementsTracker.Business.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using DTO;
+    using Interfaces;
+
+    public class UIHelper : IUIHelper
+    {
+        private ILdapAccountService ldapAccountService = new LdapAccountService();
+
+        public string GetUserFullName(string username)
+        {
+            return ldapAccountService.ReadUserFullName(username);
+        }
+
+        public IEnumerable<string> GetTags(IEnumerable<TagDTO> tags)
+        {
+            var tagTitles = new List<string>();
+
+            foreach (var tag in tags)
+            {
+                tagTitles.Add(tag.Title);
+            }
+
+            return tagTitles;
+        }
+    }
+}
