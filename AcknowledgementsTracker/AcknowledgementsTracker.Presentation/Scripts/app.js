fef5b02aa1e907e5f7712e492f42043542cc11cd
IliaAnastassov <ilia.anastassov@gmail.com>
2016-11-16 18:20:40 +0200

    Add app.js

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Scripts/app.js
@@ -0,0 +1,9 @@
+﻿loginFunctions = {
+    HideInput: function HideInput(errorLabel, usernameTxtbox, passwordTxtbox) {
+        if (errorLabel !== null || usernameTxtbox !== null || passwordTxtbox !== null) {
+            document.getElementById(errorLabel).style.display = 'none';
+            document.getElementById(usernameTxtbox).style.display = 'none';
+            document.getElementById(passwordTxtbox).style.display = 'none';
+        }
+    }
+};
