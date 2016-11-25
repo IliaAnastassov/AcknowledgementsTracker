03dcfbf1c78ee1136cc0c5a53cd5a944934d75e6
IliaAnastassov <ilia.anastassov@gmail.com>
2016-11-25 18:39:12 +0200

    Update Presentation layer

--- AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Login.aspx.cs
+++ AcknowledgementsTracker/AcknowledgementsTracker.Presentation/Login.aspx.cs
@@ -11,6 +11,8 @@
 
     public partial class Login : Page
     {
+        private IAccountService ldapAccountService = new LdapAccountService();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             txtbUsername.Focus();
@@ -24,24 +26,28 @@
                 ILdapSettingsService settings = new LdapSettingsService();
                 SetLdapSettings(settings);
 
-                try
-                {
-                    ILdapServerConnection ldapConnection = new LdapServerConnection(settings);
+                ILdapServerConnection ldapConnection = new LdapServerConnection(settings);
+                ldapConnection.Connect();
 
-                    if (ldapConnection.IsAuthenticated)
-                    {
-                        AddAuthentication(ldapConnection);
-                        Response.Redirect(Global.DashboardPage);
-                    }
-                    else
-                    {
-                        throw new Exception();
-                    }
-                }
-                catch (ThreadAbortException)
+                if (ldapConnection.IsAuthenticated)
                 {
+                    CreateAccountManager(ldapConnection);
+                    IUser user;
+
+                    try
+                    {
+                        user = ldapAccountService.ReadUserData(txtbUsername.Value);
+                    }
+                    catch (ArgumentException)
+                    {
+                        var username = ldapAccountService.ReadUserUsername(txtbUsername.Value);
+                        user = ldapAccountService.ReadUserData(username);
+                    }
+
+                    SignIn(user);
+                    Response.Redirect(Global.DashboardPage);
                 }
-                catch (Exception)
+                else
                 {
                     lblError.Visible = true;
                     lblError.InnerText = "Failed to authenticate. Please verify username and password";
@@ -69,17 +75,28 @@
             settings.UserPassword = txtbPassword.Value;
         }
 
-        private void AddAuthentication(ILdapServerConnection ldapConnection)
+        private void SignIn(IUser user)
         {
-            IAccountManager ldapManager = LdapAccountManager.Instance;
-            ldapManager.Setup(ldapConnection);
+            var authenticationTicket = new FormsAuthenticationTicket(
+                1,
+                user.Username,
+                DateTime.Now,
+                DateTime.Now.AddMinutes(30),
+                true,
+                $"{user.Name}:{user.Team}:{user.Email}");
 
-            var authenticationTicket = new FormsAuthenticationTicket(1, txtbUsername.Value, DateTime.Now, DateTime.Now.AddMinutes(30), true, string.Empty);
             var encryptedTicket = FormsAuthentication.Encrypt(authenticationTicket);
 
             var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
             cookie.Expires = authenticationTicket.Expiration;
+
             Response.Cookies.Add(cookie);
+        }
+
+        private void CreateAccountManager(ILdapServerConnection ldapConnection)
+        {
+            IAccountManager ldapManager = LdapAccountManager.Instance;
+            ldapManager.Setup(ldapConnection);
         }
     }
 }
