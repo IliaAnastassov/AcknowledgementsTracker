01c72b4464d3a60bf7daca7c167b8f3f2b08d065
IliaAnastassov <ilia.anastassov@gmail.com>
2016-11-29 18:25:50 +0200

    Add  AcknowledgementsDtoComparer to Business layer

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business/Logic/AcknowledgementsDtoComparer.cs
@@ -0,0 +1,19 @@
+﻿namespace AcknowledgementsTracker.Business.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using DTO;
+
+    public class AcknowledgementsDtoComparer : IEqualityComparer<AcknowledgementDTO>
+    {
+        public bool Equals(AcknowledgementDTO x, AcknowledgementDTO y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(AcknowledgementDTO acknowledgementDto)
+        {
+            return acknowledgementDto == null ? 0 : acknowledgementDto.Id;
+        }
+    }
+}
