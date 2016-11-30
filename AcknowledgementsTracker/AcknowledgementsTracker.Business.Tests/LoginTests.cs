47d067c3a4b9a2803aa6544fd6a464aa7c7f793d
IliaAnastassov <ilia.anastassov@gmail.com>
2016-11-30 18:48:24 +0200

    Add Tests for Business layer

---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business.Tests/LoginTests.cs
@@ -0,0 +1,194 @@
+﻿namespace AcknowledgementsTracker.Business.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AcknowledgementsTracker.Business.Logic;
+    using Interfaces;
+    using Rhino.Mocks;
+
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorMsgWhenPasswordIsNull()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = "username";
+            settings.Password = null;
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = "Please fill all textboxes";
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorMsgWhenPasswordIsEmplty()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = "username";
+            settings.Password = "";
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = "Please fill all textboxes";
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorMsgWhenPasswordIsWhiteSpace()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = "username";
+            settings.Password = " ";
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = "Please fill all textboxes";
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorMsgWhenUsernameIsNull()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = null;
+            settings.Password = "password";
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = "Please fill all textboxes";
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorMsgWhenUsernameIsEmpty()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = "";
+            settings.Password = "password";
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = "Please fill all textboxes";
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorMsgWhenUsernameIsWhiteSpace()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = " ";
+            settings.Password = "password";
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = "Please fill all textboxes";
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorMsgWhenUsernameNotFound()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = "bad user";
+            settings.Password = "password";
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = "Failed to authenticate. Please verify username and password";
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorMsgWhenPasswordIsWrong()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = "ianastassov";
+            settings.Password = "password";
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = "Failed to authenticate. Please verify username and password";
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldReturnEmptyErrorMsgWhenUsernameAndPasswordAreCorrect()
+        {
+            // ARRANGE
+            var mockLdapConnection = MockRepository.GenerateMock<ILdapServerConnection>();
+            var mockLdapAccountService = MockRepository.GenerateMock<IAccountService>();
+
+            var settings = new LdapSettingsService();
+            settings.Username = "ianastassov";
+            settings.Password = "HjuwG7Hf";
+            settings.ServerPath = "LDAP://ldap.proxiad.corp/dc=proxiad,dc=bg";
+            settings.SearchRoot = "LDAP://ldap.proxiad.corp/ou=People,dc=proxiad,dc=bg";
+
+            // ACT
+            ILoginService loginService = new LoginService(mockLdapConnection, mockLdapAccountService);
+            var response = loginService.Login(settings);
+
+            // ASSERT
+            var expectedErrorMsg = string.Empty;
+            Assert.AreEqual(expectedErrorMsg, response.ErrorMessage);
+        }
+    }
+}
---
+++ AcknowledgementsTracker/AcknowledgementsTracker.Business.Tests/TextNormalizationServiceTests.cs
@@ -0,0 +1,23 @@
+﻿namespace AcknowledgementsTracker.Business.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Business.Interfaces;
+    using Business.Logic;
+
+    [TestClass]
+    public class TextNormalizationServiceTests
+    {
+        [TestMethod]
+        public void VerifyTextNormalization()
+        {
+            INormalizable normalizer = new TextNormalizationService();
+
+            var text = "Français Gaëlle impôt München, Österreich. Úber à dépôt.";
+            var normalizedText = normalizer.NormalizeText(text);
+            var exprectedText = "francais gaelle impot munchen, osterreich. uber a depot.";
+
+            Assert.AreEqual(normalizedText, exprectedText);
+        }
+    }
+}
